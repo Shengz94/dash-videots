1bf11aa319adc43a01b806241055f39770fe0baf
sheng zhu <shengzhu@shengs-macbook-pro.home>
2021-02-27 17:59:33 +0100

    Project init

---
+++ src/videoPlayer.ts
@@ -0,0 +1,23 @@
+import dashjs from 'dashjs';
+
+class videoPlayer{
+    constructor(){
+    }
+
+    setContainer(divId: string){
+    }
+    open(videoUrl: string){
+    }
+    play(){
+    }
+    pause(){
+    }
+    seek(at:number) {
+    }
+    stop(){
+    }
+    destroy(){
+    }
+}
+
+export default videoPlayer
