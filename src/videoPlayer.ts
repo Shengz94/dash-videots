d07763daed7229732b32809ab4a092ad2f4c3d22
sheng zhu <shengzhu@shengs-macbook-pro.home>
2021-02-28 12:07:09 +0100

    Documentation of the videPlayer class and minor fixes

--- src/videoPlayer.ts
+++ src/videoPlayer.ts
@@ -3,10 +3,19 @@ import dashjs from 'dashjs';
 class videoPlayer{
     private player: dashjs.MediaPlayerClass;
 
+    /**
+     * Initializes player module.
+     * 
+     */
     constructor(){
         this.player = dashjs.MediaPlayer().create();
     }
 
+    /**
+     * Initializes and configure the player
+     * 
+     * @param {string} divId - The id of the div where the player will be deployed.
+     */
     setContainer(divId: string){
         let container = document.getElementById(divId);
         if(container != null){
@@ -22,10 +31,21 @@ class videoPlayer{
         }
     }
 
+    /**
+     * Opens a new video url and starts to preload the video.
+     * 
+     * @param {string} videoUrl - Video source URL.
+     */
     open(videoUrl: string){
         this.player.attachSource(videoUrl);
         this.player.preload();
     }
+
+    /**
+     * Plays the video.
+     * 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     play(){
         if(this.player.isReady()){
             if(this.player.isPaused()){
@@ -36,6 +56,11 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+    /**
+     * Pauses the video.
+     * 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     pause(){
         if(this.player.isReady()){
             if(!this.player.isPaused()){
@@ -45,7 +70,12 @@ class videoPlayer{
         else{
             throw new Error("Player has not attached the View or the source.");
         }
-    }
+    }/**
+     * Jumps to a specific time given in seconds.
+     * 
+     * @param {number} at - time in seconds to jump at.
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     seek(at:number) {
         if(this.player.isReady()){
             let duration : number = this.player.duration();
@@ -60,6 +90,12 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Stops the video
+     * 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     stop(){
         if(this.player.isReady()){
             this.player.attachSource(this.player.getSource());
@@ -69,6 +105,12 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Destroys the player and release the resources.
+     * 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     destroy(){
         if(this.player.isReady()){
             this.player.getVideoElement().remove();
@@ -78,15 +120,27 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Returns the duration of the current video.
+     * 
+     * @returns {number} 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     getDuration(): number{
         if(this.player.isReady()){
-            console.log("duration" + this.player.duration());
             return this.player.duration();            
         }
         else{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Toggles the mute of the function.
+     * 
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     toggleMute(){
         if(this.player.isReady()){
             this.player.setMute(!this.player.isMuted());
@@ -95,6 +149,14 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Turns the volume up/down by the value given.
+     * 
+     * @param {string} direction - It tells the direction of the volume change. "up" or "down".
+     * @param {string} value - Value to change. It's ranged from 0 to 1 (values under 0 or over 1 will set it to the min/max respectively).
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     turnVolume(direction: string, value: number){
         if(this.player.isReady()){
             if(direction === "up" && this.player.getVolume() != 1){
@@ -118,9 +180,22 @@ class videoPlayer{
             throw new Error("Player has not attached the View or the source.");
         }
     }
+
+    /**
+     * Sets the DRM protection module.
+     * Protection data should be set before the video is initialized or PROTECTION_CREATED event fired
+     * 
+     * @param {ProtectionDataSet} value - object containing
+     * property names corresponding to key system name strings and associated
+     * values being instances of.
+     * @throws {Error} - Error if there are no view and/or source attached to the player.
+     */
     setProtection(protectionData: dashjs.ProtectionDataSet){
         if(this.player.isReady()){
             this.player.setProtectionData(protectionData);
+        }
+        else{
+            throw new Error("Player has not attached the View or the source.");
         }
     }
 }
